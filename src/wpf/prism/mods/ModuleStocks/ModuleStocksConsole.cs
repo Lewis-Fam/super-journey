27d168bb20b3eb9808da4c3a3e3007b1220bb83e
alltdl <alltdl@gmail.com>
2021-06-21 14:16:23 -0600

    Update

--- src/wpf/prism/mods/ModuleStocks/ModuleStocksConsole.cs
+++ src/wpf/prism/mods/ModuleStocks/ModuleStocksConsole.cs
@@ -6,22 +6,22 @@ using LewisFam.Stocks.ThirdParty.Webull.Models;
 
 namespace ModuleStocks
 {
-    public abstract class StocksModuleConsole : BaseLewisFamConsole
-    {
-        private IEnumerable<Stock> _stocks;
+    //public abstract class StocksModuleConsole : BaseLewisFamConsole
+    //{
+    //    private IEnumerable<Stock> _stocks;
 
-        protected virtual void PrintStockSymbols()
-        {
-            if (_stocks == null)
-            {
-                PrintLine("Stocks == null");
-                return;
-            }
+    //    protected virtual void PrintStockSymbols()
+    //    {
+    //        if (_stocks == null)
+    //        {
+    //            PrintLine("Stocks == null");
+    //            return;
+    //        }
 
-            foreach (var stock in _stocks)
-            {
-                PrintLine(stock.Symbol);
-            }
-        }
-    }
+    //        foreach (var stock in _stocks)
+    //        {
+    //            PrintLine(stock.Symbol);
+    //        }
+    //    }
+    //}
 }

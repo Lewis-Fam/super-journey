d8697182e5a7cb1bd516316cb5daae1417870361
alltdl <alltdl@gmail.com>
2021-06-23 23:33:35 -0600

    reorg

---
+++ src/wpf/prism/allTdL.Core/Utils/FileUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace allTdL.Utils
+{
+    public class FileUtil
+    {
+        public static FileUtil Default = new FileUtil();
+
+        public void Move(string source, string destination)
+        {
+        }
+
+        public void Copy(string source, string destination)
+        {
+        }
+
+        public void Rename(string oldValue, string newValue)
+        {
+        }
+
+        public void Delete(string path)
+        {
+        }
+    }
+}
